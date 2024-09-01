6ac101aef54727f87d1111d1a43fc03de782815d
Ayoub Errazki <ayouberrazki1995@gmail.com>
2024-09-01 21:21:29 +0200

    TDD - Add first Unit Test - Check Return Response from GPT

--- EducateAPI.LoadGPTService/EducateAPI.LoadGPTService.Tests/GPTUnitTest.cs
+++ EducateAPI.LoadGPTService/EducateAPI.LoadGPTService.Tests/GPTUnitTest.cs
@@ -8,7 +8,19 @@ namespace EducateAPI.LoadGPTService.Tests
         [Fact]
         public async Task GetResponseAsync_ShouldReturnResponse()
         {
+            var prompt = "Are you alive ?";
+            var expectedResponse = "No, I have no life, I'm a machine thoughts.";
 
+            var mockAPIClient = new Mock<IGPTClient>();
+            mockAPIClient.Setup(client => client.GetResponseAsync(prompt))
+                            .ReturnsAsync(expectedResponse);
+
+            var gptServiceClient = mockAPIClient.Object;
+
+            //Act
+            var result = await mockAPIClient.Object.GetResponseAsync(prompt);
+
+            Assert.Equal(expectedResponse, result);
         }
     }
 }
